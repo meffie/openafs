151129062dea10521fa4376ec75d250258cf9af5
Jeffrey Altman <jaltman@auristor.com>
2018-03-24 01:22:54 -0400

    volser: DoVolDelete returning VNOVOL is success
    When moving, copying or releasing volumes, do not treat a failure
    to delete a volume because the volume no longer exists as an error.
    The volume clone has flags
    
      VTDeleteOnSalvage | VTOutOfService
    
    assigned to it which means that the fileserver won't attach the volume
    and volume has its deleteMe field assigned the value of DESTROY_ME.
    Such a volume will be deleted the next time the salvager scans the
    partition.  Once the transaction is complete the volume might be
    removed.
    
    Reviewed-on: https://gerrit.openafs.org/12976
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    (cherry picked from commit 328590dc5669cae3db6c509871b612b0384ea33d)
    
    Reviewed-on: https://gerrit.openafs.org/13235
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Reviewed-by: Mark Vitale <mvitale@sinenomine.net>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    (cherry picked from commit 6b119a79a0dce1448e21cc14be8aa1db823f6513)
    
    Change-Id: I068f37c7a697f0663a57f870965a0ce26ace53bf

--- src/volser/vsprocs.c
+++ src/volser/vsprocs.c
@@ -1897,13 +1897,12 @@ UV_MoveVolume2(afs_uint32 afromvol, afs_uint32 afromserver, afs_int32 afrompart,
     if (!(flags & RV_NOCLONE)) {
 	code = DoVolDelete(fromconn, newVol, afrompart,
 			   "cloned", 0, NULL, NULL);
-	if (code) {
-	    if (code == VNOVOL) {
-		EPRINT1(code, "Failed to start transaction on %u\n", newVol);
-	    }
+	if (code && code != VNOVOL) {
 	    error = code;
 	    goto mfail;
 	}
+
+	code = 0;	/* clone missing? that's okay */
     }
 
     /* fall through */
@@ -2509,13 +2508,12 @@ cpincr:
     if (!(flags & RV_NOCLONE)) {
 	code = DoVolDelete(fromconn, cloneVol, afrompart,
 			   "cloned", 0, NULL, NULL);
-	if (code) {
-	    if (code == VNOVOL) {
-		EPRINT1(code, "Failed to start transaction on %u\n", cloneVol);
-	    }
+	if (code && code != VNOVOL) {
 	    error = code;
 	    goto mfail;
 	}
+
+	code = 0;		/* clone missing? that's ok */
     }
 
     if (!(flags & RV_NOVLDB)) {
@@ -2641,9 +2639,6 @@ cpincr:
     if (cloneVol) {
 	code = DoVolDelete(fromconn, cloneVol, afrompart,
 			   "clone", 0, NULL, "Recovery:");
-	if (code == VNOVOL) {
-	    EPRINT1(code, "Recovery: Failed to start transaction on %u\n", cloneVol);
-	}
     }
 
   done:			/* routine cleanup */
@@ -4255,7 +4250,8 @@ UV_ReleaseVolume(afs_uint32 afromvol, afs_uint32 afromserver,
 	}
 	code = DoVolDelete(fromconn, cloneVolId, afrompart, NULL, 0, NULL,
 			   NULL);
-	ONERROR(code, cloneVolId, "Failed to delete volume %u.\n");
+	if (code && code != VNOVOL)
+	    ONERROR(code, cloneVolId, "Failed to delete volume %u.\n");
 	VDONE;
     }
 
