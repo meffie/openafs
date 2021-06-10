476bb9dee10c60f1b000797fb5dd5ceeddb81084
Michael Meffie <mmeffie@sinenomine.net>
2021-06-10 15:49:24 -0400

    [phase1] [prep] bozo: Move file renames and global init out of ReadBozoFile()
    Move the code to rename the BozoInit and BosConfig.new files,
    and the code to initialize the bosserver restart and ne out
    of the ReadBozoFile() function and to the bosserver main() function.
    
    These file renames only happen on bosserver startup so they are better
    suited to be in the main() function, and moving them out of
    ReadBozoFile() makes it easier to create unit tests for that function in
    future commits.
    
    wip: move ktime init out of read
    
    Change-Id: Ib04171eee3b0b0fd2b638111e95ccbe807ce83f5

--- src/bozo/bosprototypes.h
+++ src/bozo/bosprototypes.h
@@ -40,7 +40,8 @@ int bnode_IsRestartRequired(char *command_line, time_t last_start);
 /* bosserver.c */
 void bozo_Log(const char *format, ... );
 int bozo_ReBozo(void);
-int WriteBozoFile(char *aname);
+int ReadBozoFile(const char *aname);
+int WriteBozoFile(const char *aname);
 int bozo_CreatePidFile(char *ainst, char *aname, pid_t apid);
 int bozo_DeletePidFile(char *ainst, char *aname);
 
