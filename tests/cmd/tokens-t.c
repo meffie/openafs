49db635020f4ea27288a5353d979141b726d5d0c
Michael Meffie <mmeffie@sinenomine.net>
2021-06-23 16:10:38 -0400

    cmd: Add cmd_Split() and cmd_Join() to split and join command lines
    cmd_Split() will split a shell-like command line into an argument vector
    suitable for exec*()-style functions, without artificial limits on the
    size of arguments or the number of arguments.  cmd_Split() supports
    argument quoting and nested quoting, so whitespace can be included in
    arguments.  Both single quotes (') and double quotes (") are supported.
    For example, the string:
    
      "don't worry" be happy
    
    is split into the three strings:
    
      don't worry
      be
      happy
    
    cmd_Split() also supports backslash ('\') escaping on unix-like
    platforms.  Backslash '\' escaping is not supported on Windows, as in
    conventional, to avoid issues with path separators on that platform.
    
    The cmd_FreeSplit() may be used to free the resulting cmd_Split()
    argument vector.  (Note the existing cmd_FreeArgv() only frees the
    strings of an argv vector.)
    
    cmd_Join() is the complement of cmd_Split(). The cmd_Join() function
    will combine the strings in an argument vector to create a properly
    quoted shell-like command string, without artificial limits on the size
    of the arguments or the number of arguments.
    
    This commit includes a set of c-tap tests to put these new functions
    through their paces.
    
    Change-Id: I7eb3771aceb81dd91b1a7235c6c03a382815bdd7

---
+++ tests/cmd/tokens-t.c
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2021, Sine Nomine Associates and others.
+ * All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR `AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Tests for cmd_Split() and cmd_Join().
+ */
+
+#include <afsconfig.h>
+#include <afs/param.h>
+#include <roken.h>
+#include <afs/cmd.h>
+#include <tests/tap/basic.h>
+
+struct good_case {
+    char *text;
+    char *quoted; /* Normalized quoted. */
+    int argc;
+    char *argv[16];
+} good_cases[] = {
+    {"", NULL, 0, {NULL}},
+    {"a", NULL, 1, {"a", NULL}},
+    {"a bc def", NULL, 3, {"a", "bc", "def", NULL}},
+
+    {"''", "", 1, {"", NULL}},
+    {"\"\"", "", 1, {"", NULL}},
+    {"''\"\"''", "", 1, {"", NULL}},
+
+    {"\"don't worry\", be happy.", "'don'\"'\"'t worry,' be happy.",
+	3, {"don't worry,", "be", "happy.", NULL}},
+    {"'a b'", NULL, 1, {"a b", NULL}},
+    {"\"a b\"", "'a b'", 1, {"a b", NULL}},
+    {"'a'\"'\"'b'", NULL, 1, {"a'b", NULL}},
+    {"'a'\"'\"'b cd'", "'a'\"'\"'b cd'", 1, {"a'b cd", NULL}},
+    {"a 'bc def'", NULL, 2, {"a", "bc def", NULL}},
+    {"a \"bc def\"", "a 'bc def'", 2, {"a", "bc def", NULL}},
+    {"a 'b c' def", NULL, 3, {"a", "b c", "def", NULL}},
+    {"a b\" \"c def", "a 'b c' def", 3, {"a", "b c", "def", NULL}},
+    {"a''b", "ab", 1, {"ab", NULL}},
+    {"a b 'c \"d e\"f' g", NULL, 4, {"a", "b", "c \"d e\"f", "g", NULL}},
+    {"a b \"c 'de\" g", "a b 'c '\"'\"'de' g", 4, {"a", "b", "c 'de", "g", NULL}},
+
+#ifdef CMD_BACKSLASH_ESCAPE
+    {"a\\ b", "'a b'", 1, {"a b", NULL}},
+    {"a \\'bc", "a ''\"'\"'bc'", 2, {"a", "'bc", NULL}},
+    {"a \\\"bc", "a '\"bc'", 2, {"a", "\"bc", NULL}},
+    {"a b\\'c", "a 'b'\"'\"'c'", 2, {"a", "b'c", NULL}},
+    {"a b\\\"c", "a 'b\"c'", 2, {"a", "b\"c", NULL}},
+    {"a 'b\\ c'", NULL, 2, {"a", "b\\ c", NULL}},
+    {"a \"b\\ c\"", "a 'b\\ c'", 2, {"a", "b\\ c", NULL}},
+    {"a 'b\\\"c'", NULL, 2, {"a", "b\\\"c", NULL}},
+#else
+    {"a\\ b", "'a\\' b", 2, {"a\\", "b", NULL}},
+    {"a \\'bc'", "a '\\bc'", 2, {"a", "\\bc", NULL}},
+    {"a \\\"bc\"", "a '\\bc'", 2, {"a", "\\bc", NULL}},
+    {"a b\\'c'", "a 'b\\c'", 2, {"a", "b\\c", NULL}},
+    {"a b\\\"c\"", "a 'b\\c'", 2, {"a", "b\\c", NULL}},
+    {"a 'b\\ c'", NULL, 2, {"a", "b\\ c", NULL}},
+    {"a \"b\\ c\"", "a 'b\\ c'", 2, {"a", "b\\ c", NULL}},
+    {"a 'b\\\"c'", NULL, 2, {"a", "b\\\"c", NULL}},
+#endif
+
+    {NULL, NULL, 0, {NULL}}, /* End of tests. */
+};
+
+struct bad_case {
+    char *text;
+    int code;
+} bad_cases[] = {
+    {"'", CMD_MISSINGQUOTE},
+    {"\"", CMD_MISSINGQUOTE},
+    {"\"bcdef", CMD_MISSINGQUOTE},
+    {"'bcdef", CMD_MISSINGQUOTE},
+    {"'\"\"bcd", CMD_MISSINGQUOTE},
+    {"a bcd 'ef", CMD_MISSINGQUOTE},
+
+#ifdef CMD_BACKSLASH_ESCAPE
+    {"abc\\", CMD_BADESCAPE},
+    {"ab'c\\", CMD_MISSINGQUOTE},
+#else
+    {"abc\\'", CMD_MISSINGQUOTE},
+    {"ab\\'c", CMD_MISSINGQUOTE},
+#endif
+
+    {NULL, 0}, /* End of tests. */
+};
+
+void
+test_split(void)
+{
+    int code;
+    int tc;
+    char **tv;
+
+    for (struct good_case *t = good_cases; t->text; t++) {
+	tc = -1;
+	tv = NULL;
+	code = cmd_Split(t->text, &tc, &tv);
+	is_int(code, 0, "cmd_Split: %s", t->text);
+	if (tv == NULL)
+	    bail("cmd_Split() failed code=%d text=%s", code, t->text);
+	is_int(tc, t->argc, ".. size is %d", t->argc);
+	for (int i = 0; i <= t->argc && i <= tc; i++) {
+	    is_string(tv[i], t->argv[i], ".. argv[%d] is %s", i, t->argv[i]);
+	}
+	cmd_FreeSplit(&tv);
+    }
+
+    for (struct bad_case *t = bad_cases; t->text; t++) {
+	tc = -1;
+	tv = NULL;
+	code = cmd_Split(t->text, &tc, &tv);
+	is_int(code, t->code, "cmd_Split: %s", t->text);
+	cmd_FreeSplit(&tv);
+    }
+}
+
+void
+test_join(void)
+{
+    int code;
+
+    for (struct good_case *t = good_cases; t->text; t++) {
+	char *text = NULL;
+	char *want = t->quoted ? t->quoted : t->text;
+
+	code = cmd_Join(t->argc, t->argv, &text);
+	if (code != 0)
+	    is_int(code, 0, "cmd_Join: %s", want);
+	else
+	    is_string(text, want, "cmd_Join: %s", want);
+	free(text);
+    }
+}
+
+int
+main(int argc, char **argv)
+{
+    plan(160);
+    initialize_CMD_error_table();
+    test_split();
+    test_join();
+    return 0;
+}
