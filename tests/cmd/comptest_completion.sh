fb5bcd00797542c61ef20c78809b7bf2d178209a
Alexandra Minca <alexandraminca30@gmail.com>
2026-06-30 03:41:21 +0300

    tests/cmd: Add completion helper function
    Refactor the script to call a new helper function that
    returns the list of possible completion words using the
    previous and current word. This prepares the script for
    a future replacement of the helper with a real call to
    ./comptest <new-option>.
    
    Change-Id: I8eb963eea8dc1d92085e1612657f8d92a855dc2f

--- tests/cmd/comptest_completion.sh
+++ tests/cmd/comptest_completion.sh
@@ -1,34 +1,40 @@
+_comptest_completion_commands()
+{
+	local prev="$1"
+	local cur="$2"
+
+	case "$prev" in
+                "./comptest")
+                        echo "apropos bar foo help version"
+                        ;;
+                "foo")
+                        echo "-example -help"
+                        ;;
+                "bar")
+                        echo "-option -help"
+                        ;;
+                "version")
+                        echo "-help"
+                        ;;
+                "help"|"apropos")
+                        echo "-help -topic"
+                        ;;
+                "-example"|"-option"|"-topic")
+                        if [[ "$cur" == -* ]]; then
+                                echo "-help"
+                        fi
+                        ;;
+
+        esac
+}
+
 _comptest_completion()
 {
 	local cur="${COMP_WORDS[COMP_CWORD]}"
 	local prev="${COMP_WORDS[COMP_CWORD-1]}"
-	local commands="apropos bar foo help version"
-	local commands_foo="-example -help"
-	local commands_bar="-option -help"
 
-	case "$prev" in
-		"./comptest")
-			COMPREPLY=( $(compgen -W "$commands" -- "$cur") )
-			;;
-		"foo")
-			COMPREPLY=( $(compgen -W "$commands_foo" -- "$cur") )
-			;;
-		"bar")
-			COMPREPLY=( $(compgen -W "$commands_bar" -- "$cur") )
-			;;
-		"version")
-			COMPREPLY=( $(compgen -W "-help" -- "$cur") )
-                        ;;
-		"help"|"apropos")
-                        COMPREPLY=( $(compgen -W "-help -topic" -- "$cur") )
-                        ;;
-		"-example"|"-option"|"-topic")
-			if [[ "$cur" == -* ]]; then
-                                COMPREPLY=( $(compgen -W "-help" -- "$cur") )
-			fi
-			;;
-
-	esac
+	local cmd_list=$(_comptest_completion_commands "$prev" "$cur")
+	COMPREPLY=( $(compgen -W "$cmd_list" -- "$cur") )
 }
 
 complete -F _comptest_completion ./comptest
