5c6accff12e1e9d14d25b797213a151578297cb3
Michael Meffie <mmeffie@sinenomine.net>
2026-08-06 07:36:14 -0400

    Register completions for fs, vos, pts, and bos
    Instead of requiring an argument, register the bash completions for the
    commands we support.
    
    Change-Id: Id848e2de530debb9fc0d94e0d23deeee7b521f52

--- src/tools/completions/openafs_completion.sh
+++ src/tools/completions/openafs_completion.sh
@@ -1,24 +1,23 @@
-# This script registers the same completion function for different OpenAFS
+# This script registers the completion function for different OpenAFS
 # commands. The completion function asks the command being completed for
 # possible completions by calling its -completion-helper option.
 #
-# For manual testing, source this file once for each command name:
-#   $ source src/tools/completions/openafs_completion.sh fs
-#   $ source src/tools/completions/openafs_completion.sh vos
+# Source this file to register the completions.
+#
+#   $ source openafs_completion.sh
+#
 
 _openafs_cmd_completion()
 {
-    # COMP_WORDS and COMP_CWORD are set by Bash before calling this function.
-    # COMP_CWORD is the index of the word being completed.
-    # COMP_WORDS is the array of typed words.
     local cur="${COMP_WORDS[COMP_CWORD]}"
-    # $1 is the command name, passed by Bash when completion is triggered.
-    local cmd="$1"
+    local cmd="$1" # $1 is the command name
 
-    local cmd_list=$("$cmd" -completion-helper -- "$COMP_CWORD" "${COMP_WORDS[@]}" 2>/dev/null)
-    # COMPREPLY is the list of completion candidates that Bash displays.
+    local cmd_list=$("$cmd" -completion-helper -- "$COMP_CWORD" "${COMP_WORDS[@]}")
     COMPREPLY=( $(compgen -W "$cmd_list" -- "$cur") )
 }
 
-# $1 here is the command name passed when sourcing this file.
-complete -F _openafs_cmd_completion "$1"
+# Register command completions.
+complete -F _openafs_cmd_completion bos
+complete -F _openafs_cmd_completion fs
+complete -F _openafs_cmd_completion pts
+complete -F _openafs_cmd_completion vos
