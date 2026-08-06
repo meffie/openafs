333bc0b0f00ca93de09e04c9c3ae69275daacb4a
Michael Meffie <mmeffie@sinenomine.net>
2026-08-06 07:30:06 -0400

    cmd: Fix whitespace in CompletionHelper()
    Cleanup the whitespace and indentation
    
    Change-Id: Iaa908f81de0469afc5b7f54fb4430fd2c8c67bf8
    Note: This change should be squashed later.

--- src/tools/completions/openafs_completion.sh
+++ src/tools/completions/openafs_completion.sh
@@ -8,16 +8,16 @@
 
 _openafs_cmd_completion()
 {
-	# COMP_WORDS and COMP_CWORD are set by Bash before calling this function.
-	# COMP_CWORD is the index of the word being completed.
-	# COMP_WORDS is the array of typed words.
-	local cur="${COMP_WORDS[COMP_CWORD]}"
-	# $1 is the command name, passed by Bash when completion is triggered.
+    # COMP_WORDS and COMP_CWORD are set by Bash before calling this function.
+    # COMP_CWORD is the index of the word being completed.
+    # COMP_WORDS is the array of typed words.
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    # $1 is the command name, passed by Bash when completion is triggered.
     local cmd="$1"
 
-	local cmd_list=$("$cmd" -completion-helper -- "$COMP_CWORD" "${COMP_WORDS[@]}" 2>/dev/null)
-	# COMPREPLY is the list of completion candidates that Bash displays.
-	COMPREPLY=( $(compgen -W "$cmd_list" -- "$cur") )
+    local cmd_list=$("$cmd" -completion-helper -- "$COMP_CWORD" "${COMP_WORDS[@]}" 2>/dev/null)
+    # COMPREPLY is the list of completion candidates that Bash displays.
+    COMPREPLY=( $(compgen -W "$cmd_list" -- "$cur") )
 }
 
 # $1 here is the command name passed when sourcing this file.
